493f8d255ed89097fe563f11a522be6203fddbc3
h3kar360 <80110177+h3kar360@users.noreply.github.com>
2024-11-23 20:42:06 +0700

    added board

--- battleShipLogic.js
+++ battleShipLogic.js
@@ -1,1 +1,33 @@
-ECHO is on.
+document.addEventListener('DOMContentLoaded', () => {
+    //board
+    const board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    
+    //initialise imports
+    const container = document.getElementById('board');
+    
+
+    const createBoard = () => {
+        for (let row = 0; row < board[0].length; row++) {
+            for (let cell = 0; cell < board[0].length; cell++) {
+                if(board[row][cell] === 0){
+                    const createCell = document.createElement('div');
+                    createCell.classList.add('cell');
+                    container.appendChild(createCell);
+                }
+            }
+        }
+    }
+
+    createBoard();
+    
+    
+});
