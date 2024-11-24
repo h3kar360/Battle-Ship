e9e9fee2bec65c966932775beeca37a44e3e0c1a
h3kar360 <80110177+h3kar360@users.noreply.github.com>
2024-11-24 22:56:11 +0700

    finalise hovering and added selection

--- battleShipLogic.js
+++ battleShipLogic.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     //board
     const board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
     
     //initialise imports
     const container = document.getElementById('board');
     const dock = document.getElementById('ship-dock');
     //coordinates
-    let x;
-    let y;
+    let x = [null, null];
+    let y = [null, null];
+    //ship variables
+    let currentShip;
+    let currentShipSize = 0;
 
     //ship class
     class Ship{
@@ -25,13 +28,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         hoverBoard(){
-            if((y + this.size - 1) < board[0].length){
+            if((y[1] + this.size - 1) < board[0].length){
                 for (let i = 0; i < this.size; i++) {
-                    board[y + i][x] = 1;
-                }                
-                console.log(board);
-                
+                    if(board[y[1] + i][x[1]] !== 2){
+                        board[y[1] + i][x[1]] = 1;
+                    }                    
+                }    
+                x[0] = x[1];
+                y[0] = y[1];              
             }            
+        }
+
+        awayCell(){
+            for (let i = 0; i < this.size; i++) {
+                if(board[y[0] + i][x[0]] !== 2){
+                    board[y[0] + i][x[0]] = 0;
+                }
+            }    
+        }
+
+        selectCell(){
+            for (let i = 0; i < this.size; i++) {
+                if(board[y[1] + i][x[1]] !== 2){
+                    board[y[1] + i][x[1]] = 2;
+                }  
+            }             
         }
     }
 
@@ -42,6 +63,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const Submarine = new Ship(3);
     const Destroyer = new Ship(2);
 
+    //event listener to identify ship
+    dock.addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case "carrier":
+                currentShip = Carrier;
+                currentShipSize = Carrier.size;
+                break;
+            case "battleship":
+                currentShip = Battleship;
+                currentShipSize = Battleship.size;
+                break;
+            case "cruiser":
+                currentShip = Cruiser;
+                currentShipSize = Cruiser.size;
+                break;
+            case "submarine":
+                currentShip = Submarine;
+                currentShipSize = Submarine.size;
+                break;
+            case "destroyer":
+                currentShip = Destroyer;
+                currentShipSize = Destroyer.size;
+                break;
+            default:
+                currentShip = null;
+                currentShipSize = 0;
+        }
+    });
+
     //create board
     const createBoard = () => {
         for (let row = 0; row < board[0].length; row++) {
@@ -51,30 +101,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 if(board[row][cell] === 0){                    
                     dock.addEventListener('click', (e) => {
                         createCell.addEventListener('mouseover', () => {
-                            x = cell;
-                            y = row;
+                            x[1] = cell;
+                            y[1] = row;
 
-                            if(e.target.id === "carrier"){
-                                Carrier.hoverBoard();
-                                updateBoard();
-                            }
-                            else if(e.target.id === "battleship"){
-                                Battleship.hoverBoard();
-                                updateBoard();
-                            }
-                            else if(e.target.id === "cruiser"){
-                                Cruiser.hoverBoard();
-                                updateBoard();
-                            }
-                            else if(e.target.id === "submarine"){
-                                Submarine.hoverBoard();
-                                updateBoard();
-                            }
-                            else if(e.target.id === "destroyer"){
-                                Destroyer.hoverBoard();
-                                updateBoard();
-                            }                            
+                            currentShip.hoverBoard();                    
+                            updateBoard();                            
                         });
+
+                        createCell.addEventListener('mouseout', () => {
+                            currentShip.awayCell();
+                            updateBoard();
+                        });
+
+                        createCell.addEventListener('click', () => {
+                            currentShip.selectCell();
+                            updateBoard();
+                        })
                     });
                     container.appendChild(createCell);
                 }
@@ -88,10 +130,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         //shade cells when certain ships hovered
         for (let row = 0; row < board[0].length; row++) {
-            for (let cell = 0; cell < board[0].length; cell++) {                
-                if(board[row][cell] === 1){                    
+            for (let cell = 0; cell < board[0].length; cell++) { 
+                if(board[row][cell] === 0){                    
+                    cellDiv[cell + row * board[0].length].style.backgroundColor = 'white';
+                }               
+                else if(board[row][cell] === 1){                    
                     cellDiv[cell + row * board[0].length].style.backgroundColor = 'red';
                 }
+                else if(board[row][cell] === 2){
+                    cellDiv[cell + row * board[0].length].style.backgroundColor = 'grey';
+                }                
             }
         }
         
